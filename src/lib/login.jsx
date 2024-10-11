56d48f7800cd58b2549fcd37b5afe617a81b8749
kathleencorreia <kathleencorreia13@gmail.com>
2024-10-11 08:35:20 -0300

    teste

--- src/lib/login.jsx
+++ src/lib/login.jsx
@@ -1,9 +1,5 @@
 import login from 'axios';
 
 export const api = login.create({
-    baseURL: import.meta.env.VITE_API_URL_LOGIN, // Use import.meta.env para acessar variáveis de ambiente no Vite
+    baseURL: process.env.REACT_APP_API_URL_LOGIN,
 });
-
-if (!import.meta.env.VITE_API_URL_LOGIN) {
-    console.error("A variável de ambiente VITE_API_URL_LOGIN não está definida.");
-}
