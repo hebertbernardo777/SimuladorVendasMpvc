76bf5a073e3bf674cbd6a3ab2385f1bec1a403dc
kathleencorreia <kathleencorreia13@gmail.com>
2024-10-18 11:06:05 -0300

    fix: configurando proxy no vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://appmpvc.com.br:3333',  // URL da sua API sem HTTPS
-        changeOrigin: true,  // Ajusta o cabeçalho Host na requisição para corresponder ao target
-        rewrite: (path) => path.replace(/^\/api/, '')  // Remove o prefixo /api do caminho
-      }
-    }
-  }
-})
+      "/api": {
+        target: "http://appmpvc.com.br:3333", // URL da sua API sem HTTPS
+        changeOrigin: true, // Ajusta o cabeçalho Host na requisição para corresponder ao target
+        rewrite: (path) => path.replace(/^\/api/, ""), // Remove o prefixo /api do caminho
+      },
+      "/parametros": {
+        target: "http://appmpvc.com.br:3333", // URL da sua API sem HTTPS
+        changeOrigin: true, // Ajusta o cabeçalho Host na requisição para corresponder ao target
+        rewrite: (path) => path.replace(/^\/parametros/, ""), // Remove o prefixo /api do caminho
+      },
+      "/tabela": {
+        target: "http://appmpvc.com.br:3333", // URL da sua API sem HTTPS
+        changeOrigin: true, // Ajusta o cabeçalho Host na requisição para corresponder ao target
+        rewrite: (path) => path.replace(/^\/tabela/, ""), // Remove o prefixo /api do caminho
+      },
+      "/carteiraclientes": {
+        target: "http://appmpvc.com.br:3333", // URL da sua API sem HTTPS
+        changeOrigin: true, // Ajusta o cabeçalho Host na requisição para corresponder ao target
+        rewrite: (path) => path.replace(/^\/carteiraclientes/, ""), // Remove o prefixo /api do caminho
+      },
+      "/rotas": {
+        target: "http://appmpvc.com.br:3333", // URL da sua API sem HTTPS
+        changeOrigin: true, // Ajusta o cabeçalho Host na requisição para corresponder ao target
+        rewrite: (path) => path.replace(/^\/rotas/, ""), // Remove o prefixo /api do caminho
+      },
+    },
+  },
+});
